d925937aea5f9cd7ac680145d8185e9b3a408769
configtheworld <erkinture@gmail.com>
2022-02-02 04:06:10 +0300

    mongo connection router added

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.AUTH_PORT || 3001;
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/auth-service', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB configed');
+  })
+  .catch((err) => {
+    console.log('ERROR:', err.message);
+  });
 
 app.use(express.json());
 
---
+++ auth-service/user_routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/auth/register', (req, res) => {
+  // TODO controller
+});
+
+router.post('/auth/login', (req, res) => {
+  // TODO controller
+});
+
+module.exports = router;
