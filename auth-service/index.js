17430ca18eacfecbec7e98932075792cd68986f0
configtheworld <erkinture@gmail.com>
2022-02-03 02:01:52 +0300

    authentication routes added

--- auth-service/index.js
+++ auth-service/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.AUTH_PORT || 3001;
 const mongoose = require('mongoose');
+const user_routes = require('./user_routes');
 
 mongoose
   .connect('mongodb://localhost/auth-service', {
@@ -16,6 +17,8 @@ mongoose
   });
 
 app.use(express.json());
+
+app.use(user_routes);
 
 app.listen(PORT, () => {
   console.log('auth service is on live');
--- auth-service/user_routes.js
+++ auth-service/user_routes.js
@@ -1,12 +1,69 @@
 const express = require('express');
+const User = require('./User');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
 
-router.post('/auth/register', (req, res) => {
-  // TODO controller
+router.get('/ping', (req, res) => {
+  res.send('pong');
 });
 
-router.post('/auth/login', (req, res) => {
-  // TODO controller
+router.post('/auth/register', async (req, res) => {
+  const { email, password, name } = req.body;
+
+  try {
+    const emailTaken = await User.findOne({ email });
+    if (emailTaken) {
+      return res.json({ message: 'Mail Already Taken!' });
+    } else {
+      const hash = await bcrypt.hash(password, 10);
+      const new_user = new User({
+        name: name,
+        email: email,
+        password: hash,
+      });
+      new_user.save();
+      return res.json({ message: 'User Successfully Registered!' });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post('/auth/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    if (email && password) {
+      const user = await User.findOne({ email: email });
+
+      if (user) {
+        const auth_check = await bcrypt.compare(password, user.password);
+
+        if (auth_check) {
+          const jwt_payload = {
+            email,
+            name: user.name,
+          };
+
+          const token = await jwt.sign(jwt_payload, '*ExperimentalSecretCode*');
+
+          return res.json({
+            message: 'Logged In Successfully!',
+            token: token,
+          });
+        } else {
+          return res.json({
+            message: 'Wrong email or password!',
+          });
+        }
+      } else {
+        return res.json({ message: 'Wrong email or password!' });
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
